4f30c82ff63ea5eeef88460088623093ce0c9654
Aya-Y07 <7637@greengates.edu.mx>
2022-04-01 11:55:15 -0600

    Initial commit

--- app/src/main/java/mx/edu/greengates/ia_question_app/profiles.java
+++ app/src/main/java/mx/edu/greengates/ia_question_app/profiles.java
@@ -1,16 +1,14 @@
-package mx.edu.greengates.ia_question_app.data;
+package mx.edu.greengates.ia_question_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import mx.edu.greengates.ia_question_app.R;
-
-public class MainActivity extends AppCompatActivity {
+public class profiles extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
     }
 }
