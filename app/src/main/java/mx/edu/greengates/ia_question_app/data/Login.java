b4587bdaa50030aae99cb59895c91fe382417293
Daniel Vazquez <dvazquez@greengates.edu.mx>
2022-04-04 14:30:01 -0500

    Amendments to read write user credentials

--- app/src/main/java/mx/edu/greengates/ia_question_app/data/Login.java
+++ app/src/main/java/mx/edu/greengates/ia_question_app/data/Login.java
@@ -10,9 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -26,10 +29,6 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     EditText password;
     private Button login;
     private Button register;
-    private String user;
-    private String pass;
-    private boolean validCredential;
-    private byte[] encryptedPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,75 +38,103 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         username = (EditText) findViewById(R.id.input_username);
         password = (EditText) findViewById(R.id.input_password);
 
+        // This button will trigger the Login function
+        // 1) Take credentials
+        // 2) Check against storage (users.csv)
+        // 3) If credential fail request registration
         login = (Button) findViewById(R.id.btn_login);
         login.setOnClickListener(this);
 
+        // This button will trigger the Registration function
+        // 1) Request new credentials
+        // 2) Add credentials to login file
+        // 3) Return to login
         register = (Button) findViewById(R.id.btn_register);
         register.setOnClickListener(this);
 
-        user = String.valueOf(username.getText());
-        pass = String.valueOf(password.getText());
-
     }
 
-    private void encrypted() {
-
-
+    /**
+     * This function returns a SHA-1 encrypted HEX string from a given password
+     * @param pass the password to convert into SHA-1
+     * @return the encrypted password to validate
+     */
+    private String encrypted(String pass) {
+        String encryptedPass = "";
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-1");
-            encryptedPass = digest.digest(pass.getBytes(StandardCharsets.UTF_8));
+            digest.reset();
+            digest.update(pass.getBytes(StandardCharsets.UTF_8));
+            encryptedPass = String.format("%040x", new BigInteger(1, digest.digest()));
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        return encryptedPass;
     }
 
-    private boolean checkCredentials() {
-        validCredential = false;
-        AssetManager assetManager = getAssets();
-        InputStream is = null;
+    /**
+     * This function should check credentials against the storage
+     * If credentials exists and correct return true
+     * Else return false
+     * @return boolean valid credentials
+     */
+    private boolean checkCredentials(String username, String password) {
+        boolean validCredential = false;  // assume credentials are invalid
+
+        BufferedReader buffReader = null;
         try {
-            is = assetManager.open("users.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        BufferedReader reader = null;
-        reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-        String line = "";
-        StringTokenizer st = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                st = new StringTokenizer(line, ",");
-                String username = st.nextToken();
-                String password = st.nextToken();
-                if (user.compareTo(username) == 0 && encryptedPass.equals(password)) {
-                    validCredential = true;
+            String sCurrentLine;
+            File file = new File(this.getFilesDir(), "users.csv");
+            FileReader fileReader = new FileReader(file);
+            buffReader = new BufferedReader(fileReader);
+            while ((sCurrentLine = buffReader.readLine()) != null) {
+                String[] credentials = sCurrentLine.split(",");
+                String credUserName = credentials[0]; // First item is username
+                String credPassword = credentials[1]; // Second item is password
+                if(username.equals(credUserName) && password.equals(credPassword) ) {
+                    validCredential = true; // we found a match
                     break;
                 }
-
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (buffReader != null) buffReader.close(); // close the buffer
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
         return validCredential;
-
     }
 
     public void onClick(View v) {
 
-        if ((v == login && validCredential == true)) {
-            Intent myIntent = new Intent(Login.this, Home.class);
-            Login.this.startActivity(myIntent);
+        if ((v == login)) {
+            String user = String.valueOf(username.getText());
+            String pass = String.valueOf(password.getText());
+            String encryptedPass = encrypted(pass);
+            boolean valid = checkCredentials(user, encryptedPass);
+            if (valid) {
+                Intent myIntent = new Intent(Login.this, Home.class);
+                Login.this.startActivity(myIntent);
+            } else {
+                Intent myIntent = new Intent(Login.this, Profile.class);
+                String login = "L";
+                myIntent.putExtra("login", login);
+                myIntent.putExtra("user", user);
+                myIntent.putExtra("pass", pass);
+                Login.this.startActivity(myIntent);
+            }
 
         }
         if (v == register) {
             Intent myIntent = new Intent(Login.this, Profile.class);
-            Login.this.startActivity(myIntent);
             String login = "L";
             myIntent.putExtra("login", login);
+            Login.this.startActivity(myIntent);
         }
         return;
-
     }
 
 }
--- app/src/main/java/mx/edu/greengates/ia_question_app/data/Profile.java
+++ app/src/main/java/mx/edu/greengates/ia_question_app/data/Profile.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,55 +29,75 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
         private String password;
         private byte[] b;
 
-
-
         @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_profile);
 
+            username = "Username";
+            password = "Password";
+
             Intent intent = getIntent();
             confirm = intent.getStringExtra("login");
-
+            if (intent.hasExtra("user")) {
+                username = intent.getStringExtra("user");
+            }
+            if (intent.hasExtra("pass")){
+                password = intent.getStringExtra("pass");
+            }
 
             submitBtn = (Button) findViewById(R.id.btn_profile_submit);
             profile_username = (EditText) findViewById(R.id.profile_username);
+            profile_username.setText(username);
             profile_password = (EditText) findViewById(R.id.profile_password);
+            profile_password.setText(password);
 
             submitBtn.setOnClickListener(this);
-
-
-            username = profile_username.getText().toString();
-            password = profile_password.getText().toString();
-
         }
 
-    public void encryptedpasswid(){
-            try{
-                MessageDigest digest = MessageDigest.getInstance("SHA-1");
-                b = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-            }catch(Exception e) {
-                e.printStackTrace();
-            }
+    /**
+     * This function returns a SHA-1 encrypted HEX string from a given password
+     * @param pass the password to convert into SHA-1
+     * @return the encrypted password to validate
+     */
+    private String encrypted(String pass) {
+        String encryptedPass = "";
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(pass.getBytes(StandardCharsets.UTF_8));
+            encryptedPass = String.format("%040x", new BigInteger(1, digest.digest()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedPass;
     }
+
     public void onClick(View view) {
-
-
 
             if (view == submitBtn){
 
+                username = profile_username.getText().toString();
+                password = profile_password.getText().toString();
+                String encryptedPass = encrypted(password);
                 try {
+                    File file = new File(this.getFilesDir(), "users.csv");
+                    // if file doesnt exists, then create it
+                    if (!file.exists()) {
+                        file.createNewFile();
+                    }
+                    FileWriter fw = new FileWriter(file, true);
+                    BufferedWriter bw = new BufferedWriter(fw);
 
-                    FileWriter fw = new FileWriter("users.csv", true);
-                    PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+                    StringBuffer sb = new StringBuffer();
+                    sb.append(username);
+                    sb.append(",");
+                    sb.append(encryptedPass);
+                    String credential = sb.toString();
+                    Log.d("Profile", credential);
 
-
-                    pw.print(username);
-                    pw.print(",");
-                    pw.print(b);
-
-                    pw.close();
-
+                    bw.write(credential);
+                    bw.close();
 
                 } catch (IOException ex) {
                     ex.printStackTrace();
